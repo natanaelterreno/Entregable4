9ab37095af3a437b427657b640b815fa6f086aef
natanaelterreno <131265815+natanaelterreno@users.noreply.github.com>
2024-04-23 11:31:10 -0300

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import productsRouter from './routes/products.router.js'
+import cartRouter from './routes/cart.router.js'
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use('/api/products', productsRouter)
+app.use('/api/carts', cartRouter)
+
+app.listen('8080', err => {
+    console.log('Escuchando en el puerto 8080');
+})
